a9bd55cbedc582cd125fc8dee1ca1ad08fb038fb
牛加人等于朱 <niuren.zhu@icloud.com>
2017-06-02 16:11:50 +0800

    修复引用错误

--- ibas.documents.service/src/main/webapp/bsui/m/document/DocumentListView.ts
+++ ibas.documents.service/src/main/webapp/bsui/m/document/DocumentListView.ts
@@ -9,4 +9,4 @@
 import * as ibas from "ibas/index";
 import { utils } from "openui5/typings/ibas.utils";
 import * as bo from "../../../borep/bo/index";
-import { IDocumentViewView } from "../../../bsapp/document/index";
+import { IDocumentListView } from "../../../bsapp/document/index";
