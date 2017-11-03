409dd6d019a079855366200945bdb8b785c30c12
牛加人等于朱 <niuren.zhu@icloud.com>
2017-11-03 14:07:05 +0800

    解决传入查询的使用问题

--- ibas.documents.service/src/main/webapp/bsui/c/document/DocumentChooseView.ts
+++ ibas.documents.service/src/main/webapp/bsui/c/document/DocumentChooseView.ts
@@ -121,7 +121,16 @@ export class DocumentChooseView extends ibas.BOChooseView implements IDocumentCh
                 that.fireViewEvents(that.fetchDataEvent, criteria);
             }
         });
-        return new sap.m.Dialog("", {            title: this.title,            type: sap.m.DialogType.Standard,            state: sap.ui.core.ValueState.None,            stretchOnPhone: true,            horizontalScrolling: true,            verticalScrolling: true,            content: [this.table],            buttons: [this.darwBars()]        });
+        return new sap.m.Dialog("", {
+            title: this.title,
+            type: sap.m.DialogType.Standard,
+            state: sap.ui.core.ValueState.None,
+            stretchOnPhone: true,
+            horizontalScrolling: true,
+            verticalScrolling: true,
+            content: [this.table],
+            buttons: [this.darwBars()]
+        });
     }
     private table: sap.ui.table.Table;
     /** 显示数据 */
@@ -136,7 +145,7 @@ export class DocumentChooseView extends ibas.BOChooseView implements IDocumentCh
                     hDatas.rows.push(item);
                 }
                 model.refresh(false);
-                done = true;
+
             }
         }
         if (!done) {
@@ -145,11 +154,11 @@ export class DocumentChooseView extends ibas.BOChooseView implements IDocumentCh
         }
         this.table.setBusy(false);
     }
-    private lastCriteria: ibas.ICriteria;
+
     /** 记录上次查询条件，表格滚动时自动触发 */
     query(criteria: ibas.ICriteria): void {
         super.query(criteria);
-        this.lastCriteria = criteria;
+
         // 清除历史数据
         if (this.isDisplayed) {
             this.table.setBusy(true);
