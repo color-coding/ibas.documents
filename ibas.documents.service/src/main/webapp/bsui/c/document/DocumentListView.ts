cf6fe641465d5be36d7c8623f9e1d0c1c17b9caa
牛加人等于朱 <niuren.zhu@icloud.com>
2017-11-10 12:34:32 +0800

    调整openui5库路径

--- ibas.documents.service/src/main/webapp/bsui/c/document/DocumentListView.ts
+++ ibas.documents.service/src/main/webapp/bsui/c/document/DocumentListView.ts
@@ -7,7 +7,7 @@
  */
 
 import * as ibas from "ibas/index";
-import { utils } from "openui5/typings/ibas.utils";
+import * as openui5 from "openui5/index";
 import * as bo from "../../../borep/bo/index";
 import { IDocumentListView } from "../../../bsapp/document/index";
 
@@ -29,7 +29,7 @@ export class DocumentListView extends ibas.BOListView implements IDocumentListVi
         this.form = new sap.ui.layout.form.SimpleForm("");
         this.table = new sap.ui.table.Table("", {
             enableSelectAll: false,
-            visibleRowCount: ibas.config.get(utils.CONFIG_ITEM_LIST_TABLE_VISIBLE_ROW_COUNT, 15),
+            visibleRowCount: ibas.config.get(openui5.utils.CONFIG_ITEM_LIST_TABLE_VISIBLE_ROW_COUNT, 15),
             visibleRowCountMode: sap.ui.table.VisibleRowCountMode.Interactive,
             rows: "{/rows}",
             columns: [
@@ -99,7 +99,7 @@ export class DocumentListView extends ibas.BOListView implements IDocumentListVi
                         press: function (): void {
                             that.fireViewEvents(that.viewDataEvent,
                                 // 获取表格选中的对象
-                                utils.getTableSelecteds<bo.Document>(that.table).firstOrDefault()
+                                openui5.utils.getTableSelecteds<bo.Document>(that.table).firstOrDefault()
                             );
                         }
                     }),
@@ -111,7 +111,7 @@ export class DocumentListView extends ibas.BOListView implements IDocumentListVi
                         press: function (): void {
                             that.fireViewEvents(that.editDataEvent,
                                 // 获取表格选中的对象
-                                utils.getTableSelecteds<bo.Document>(that.table).firstOrDefault()
+                                openui5.utils.getTableSelecteds<bo.Document>(that.table).firstOrDefault()
                             );
                         }
                     }),
@@ -123,7 +123,7 @@ export class DocumentListView extends ibas.BOListView implements IDocumentListVi
                         press: function (): void {
                             that.fireViewEvents(that.deleteDataEvent,
                                 // 获取表格选中的对象
-                                utils.getTableSelecteds<bo.Document>(that.table)
+                                openui5.utils.getTableSelecteds<bo.Document>(that.table)
                             );
                         }
                     }),
@@ -165,7 +165,7 @@ export class DocumentListView extends ibas.BOListView implements IDocumentListVi
         });
         this.id = this.page.getId();
         // 添加列表自动查询事件
-        utils.triggerNextResults({
+        openui5.utils.triggerNextResults({
             listener: this.table,
             next(data: any): void {
                 if (ibas.objects.isNull(that.lastCriteria)) {
@@ -224,6 +224,6 @@ export class DocumentListView extends ibas.BOListView implements IDocumentListVi
     }
     /** 获取选择的数据 */
     getSelecteds(): bo.Document[] {
-        return utils.getTableSelecteds<bo.Document>(this.table);
+        return openui5.utils.getTableSelecteds<bo.Document>(this.table);
     }
 }
