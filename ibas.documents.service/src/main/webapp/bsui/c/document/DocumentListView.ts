eacce6d61bd1119f1c647f5c2d2c34591b378adf
NiurenZhu <niuren.zhu@icloud.com>
2017-11-16 20:23:14 +0800

    去除空行

--- ibas.documents.service/src/main/webapp/bsui/c/document/DocumentListView.ts
+++ ibas.documents.service/src/main/webapp/bsui/c/document/DocumentListView.ts
@@ -236,7 +236,6 @@ export class DocumentListView extends ibas.BOListView implements IDocumentListVi
     /** 记录上次查询条件，表格滚动时自动触发 */
     query(criteria: ibas.ICriteria): void {
         super.query(criteria);
-
         // 清除历史数据
         if (this.isDisplayed) {
             this.table.setBusy(true);
