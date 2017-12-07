04b3c866e09112b6935621bfd2f7017226a71aaf
niurenzhu <niuren.zhu@icloud.com>
2017-12-07 21:45:53 +0800

    完善列表删除功能

--- ibas.documents.service/src/main/webapp/bsapp/document/DocumentListApp.ts
+++ ibas.documents.service/src/main/webapp/bsapp/document/DocumentListApp.ts
@@ -99,7 +99,7 @@ export class DocumentListApp extends ibas.BOListApplication<IDocumentListView, b
         app.run(data);
     }
     /** 删除数据，参数：目标数据集合 */
-    protected deleteData(data: bo.Document): void {
+    protected deleteData(data: bo.Document | bo.Document[]): void {
         // 检查目标数据
         if (ibas.objects.isNull(data)) {
             this.messages(ibas.emMessageType.WARNING, ibas.i18n.prop("shell_please_chooose_data",
@@ -110,11 +110,12 @@ export class DocumentListApp extends ibas.BOListApplication<IDocumentListView, b
         let beDeleteds: ibas.ArrayList<bo.Document> = new ibas.ArrayList<bo.Document>();
         if (data instanceof Array) {
             for (let item of data) {
-                if (ibas.objects.instanceOf(item, bo.Document)) {
-                    item.delete();
-                    beDeleteds.add(item);
-                }
+                item.delete();
+                beDeleteds.add(item);
             }
+        } else {
+            data.delete();
+            beDeleteds.add(data);
         }
         // 没有选择删除的对象
         if (beDeleteds.length === 0) {
