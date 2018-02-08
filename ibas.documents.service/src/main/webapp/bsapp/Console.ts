0f1fec3bf993c262ff830007b33447a3a8e0d5ca
牛加人等于朱 <niuren.zhu@icloud.com>
2018-02-08 13:46:09 +0800

    合并业务对象的资源文件

--- ibas.documents.service/src/main/webapp/bsapp/Console.ts
+++ ibas.documents.service/src/main/webapp/bsapp/Console.ts
@@ -44,8 +44,7 @@ export class Console extends ibas.ModuleConsole {
     run(): void {
         // 加载语言-框架默认
         ibas.i18n.load(this.rootUrl + "resources/languages/documents.json");
-        ibas.i18n.load(this.rootUrl + "resources/languages/enums.json");
-        ibas.i18n.load(this.rootUrl + "resources/languages/bo/document.json");
+        ibas.i18n.load(this.rootUrl + "resources/languages/bos.json");
         // 设置资源属性
         this.description = ibas.i18n.prop(this.name.toLowerCase());
         this.icon = ibas.i18n.prop(this.name.toLowerCase() + "_icon");
