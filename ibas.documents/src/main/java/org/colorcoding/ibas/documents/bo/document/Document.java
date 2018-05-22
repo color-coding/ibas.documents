c19faee2a10b8f332c1c16715c8a8ce37ebc906d
牛加人等于朱 <niuren.zhu@icloud.com>
2018-05-22 15:28:24 +0800

    继承自定义字段接口

--- ibas.documents/src/main/java/org/colorcoding/ibas/documents/bo/document/Document.java
+++ ibas.documents/src/main/java/org/colorcoding/ibas/documents/bo/document/Document.java
@@ -8,6 +8,7 @@ import javax.xml.bind.annotation.XmlType;
 
 import org.colorcoding.ibas.bobas.approval.IApprovalData;
 import org.colorcoding.ibas.bobas.bo.BusinessObject;
+import org.colorcoding.ibas.bobas.bo.IBOUserFields;
 import org.colorcoding.ibas.bobas.core.IPropertyInfo;
 import org.colorcoding.ibas.bobas.data.DateTime;
 import org.colorcoding.ibas.bobas.data.emApprovalStatus;
@@ -26,7 +27,8 @@ import org.colorcoding.ibas.documents.MyConfiguration;
 @XmlType(name = Document.BUSINESS_OBJECT_NAME, namespace = MyConfiguration.NAMESPACE_BO)
 @XmlRootElement(name = Document.BUSINESS_OBJECT_NAME, namespace = MyConfiguration.NAMESPACE_BO)
 @BOCode(Document.BUSINESS_OBJECT_CODE)
-public class Document extends BusinessObject<Document> implements IDocument, IDataOwnership, IApprovalData {
+public class Document extends BusinessObject<Document>
+		implements IDocument, IDataOwnership, IApprovalData, IBOUserFields {
 
 	/**
 	 * 序列化版本标记
